f9c265168b95d83cd7854276a68788fd5fff5590
arkar20 <arkar20011@gmail.com>
2025-01-27 06:55:25 -0800

    init: pj setup, shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input";
+
+function App() {
+    return (
+        <>
+            <div className="dark bg-background w-full h-full min-h-screen text-white">
+                <Input />
+            </div>
+        </>
+    );
+}
+
+export default App;
